33696dbc8b00fc211ec858ca98ced3178f8c8dc6
Tyeshela <tyeshelatyeshela18@gmail.com>
2023-11-21 23:15:23 +0400

    Coding Challenge

---
+++ Data Type Explorer/starting code.py
@@ -0,0 +1,15 @@
+def data_type_explorer(integers, floats, strings, is_true, is_false, numbers, names):
+    # Your code here
+    pass # Remove this
+    
+# Example usage:
+integers = [5, -10, 8, 3, 12]
+floats = [3.14159, 19.99, 2.5, 7.8]
+strings = ["Alice", "Bob", "Charlie"]
+is_true = True
+is_false = False
+numbers = [1, 2, 3, 4, 5]
+names = ["Alice", "Bob", "Charlie"]
+
+result = data_type_explorer(integers, floats, strings, is_true, is_false, numbers, names)
+print(result)
---
+++ Operator Mastery/starting code.py
@@ -0,0 +1,14 @@
+def operator_mastery(x, y, a, b, p, q, text):
+    # Your code here
+    pass # Remove this
+# Example usage:
+x = 10
+y = 3
+a = 5
+b = 7
+p = True
+q = False
+text = "Hello, world!"
+
+result = operator_mastery(x, y, a, b, p, q, text)
+print(result)
